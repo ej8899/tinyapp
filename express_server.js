66069ff7a9460676c67d4fd17ee425ea9fed0029
ej8899 <ej8899@gmail.com>
2022-08-03 09:16:14 -0600

    add new URL form & handler

--- express_server.js
+++ express_server.js
@@ -5,11 +5,26 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+const makeID = function (numChars) {
+  var yourCode = "";
+  var possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  if(!numChars) {
+    numChars = 6;
+  }
+  for (var i = 0; i < numChars; i++) {
+    yourCode += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+  return yourCode;
+};
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -36,14 +51,32 @@ app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
 
+
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render("urls_index.ejs", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {  // NOTE ORDER is important
+  res.render("urls_new.ejs");
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVarsDb = { urls: urlDatabase};
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] }; 
   // <%= urls[id] %>
-  res.render("urls_show", templateVars);
+  res.render("urls_show.ejs", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  console.log(makeID(6));
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
+
+
+
+
+
