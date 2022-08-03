1c0c14e350c404aec23781276624456b63bf3d9f
ej8899 <ej8899@gmail.com>
2022-08-03 15:38:26 -0600

    update makeID w. default notes

--- express_server.js
+++ express_server.js
@@ -17,7 +17,7 @@ const makeID = function (numChars) {
   let yourCode = "";
   let possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   if (!numChars) {
-    numChars = 6;
+    numChars = 6; // set a default to 6 characters of a random ID
   }
   for (let i = 0; i < numChars; i++) {
     yourCode += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
