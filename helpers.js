fea8754bbf9a444ad49b121e83b445097fb91e52
ej8899 <ej8899@gmail.com>
2022-08-09 20:20:48 -0600

    add findUserByEmail and makeServerTitle

--- helpers.js
+++ helpers.js
@@ -15,6 +15,28 @@ const findUserByEmail = function(emailAddy,database) {
   return false;
 };
 
+
+//
+// create server title ascii art
+//
+const makeServerTitle = function() {
+  let m = conColorMagenta + conColorBright, c = conColorCyan + conColorDim, o = conColorOrange + conColorBright;
+  const consoleLine = '-'.repeat(43);
+  console.log(`\n\n  ${m} _    _                  ${c}_                 ${conColorReset}`);
+  console.log(`  ${m}| |_ (_) _ __   _   _   ${c}/_\\   _ __   _ __  ${conColorReset}`);
+  console.log(`  ${m}| __|| || '_ \\ | | | | ${c}//_\\\\ | '_ \\ | '_ \\ ${conColorReset}`);
+  console.log(`  ${m}| |_ | || | | || |_| |${c}/  _  \\| |_) || |_) |${conColorReset}`);
+  console.log(`   ${m}\\__||_||_| |_| \\__, |${c}\\_/ \\_/| .__/ | .__/ ${conColorReset}`);
+  console.log(`   ${o}__             ${m}|___/        ${c}|_|    |_|    ${conColorReset}`);
+  console.log(`  ${o}/ _\\  ___  _ __ __   __ ___  _ __          `);
+  console.log(`  \\ \\  / _ \\| '__|\\ \\ / // _ \\| '__|         `);
+  console.log(`  _\\ \\|  __/| |    \\ V /|  __/| |            `);
+  console.log(`  \\__/ \\___||_|     \\_/  \\___||_|            `);
+  console.log(conColorYellow +  conColorDim + '  ' + consoleLine);
+  console.log(conColorReset);
+};
+
 module.exports = {
   findUserByEmail,
+  makeServerTitle,
 }
